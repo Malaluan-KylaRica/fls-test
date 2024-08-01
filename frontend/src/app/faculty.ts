60e5ae11871e39dae1fda25012220636cf03c24d
malaluankylarica <146600161+malaluankylarica@users.noreply.github.com>
2024-08-01 23:54:47 +0800

    add

---
+++ frontend/src/app/faculty.ts
@@ -0,0 +1,16 @@
+export class Faculty {
+    faculty_name: string;
+    faculty_code: string;
+    faculty_password: string;
+    faculty_email: string;
+    faculty_type: string;
+  
+    constructor() {
+      this.faculty_name = '';
+      this.faculty_code = '';
+      this.faculty_password = '';
+      this.faculty_email = '';
+      this.faculty_type = '';
+    }
+  }
+  
